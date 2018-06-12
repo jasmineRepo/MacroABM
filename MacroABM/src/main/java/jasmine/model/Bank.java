eff965326044a3805559a265151eddf19a26f0cb
rer <r.richardson05@alumni.imperial.ac.uk>
2018-06-13 00:50:28 +0100

    type fix of bankDifficulty

--- MacroABM/src/main/java/jasmine/model/Bank.java
+++ MacroABM/src/main/java/jasmine/model/Bank.java
@@ -371,7 +371,7 @@ public class Bank extends Agent implements IDoubleSource {
 //			 }
 //			 this.badDebt						= 0;
 
-			collector.bankDifficulty			= 0;
+			collector.bankDifficulty			= false;
 		}
 		
 	}
